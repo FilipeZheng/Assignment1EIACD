ddaf0d2f7e391f81e1f058c7455a9d99829d0841
FilipeZheng <zhengfilipe@gmail.com>
2025-03-24 19:31:32 +0000

    Update game.py
    Fixed bug where the grid is actually 8x10
    Fixed bug where the repr function of a State was hardcoded for 7x9
    Moved initial animals into the board class

--- game.py
+++ game.py
@@ -24,7 +24,7 @@ class animal():
         def move(dir,cur_pos):          #returns None if the movement is invalid and the target pos if the movement is valid
             x,y = cur_pos
             next_pos = (x+dir[0],y+dir[1])
-            if next_pos[0]<0 or next_pos[0]>state.board.length or next_pos[1]<0 or next_pos[1]>state.board.height: return
+            if next_pos[0]<0 or next_pos[0]>=state.board.length or next_pos[1]<0 or next_pos[1]>=state.board.height: return
             if next_pos in state.animals[self.player]: return               # cannot move to a tile with an animal of the same player
             if next_pos in state.board.water:
                 if self.canJump and not next_pos in state.animals[3-self.player]: # checks in case the animal can jump and whether there is a mouse in the water
@@ -46,7 +46,7 @@ class animal():
 
 
 class board:        # used for representing the initial board, does not include the animals
-    def __init__(self,empty_board:list):
+    def __init__(self,empty_board:list,initial_p1_animals,initial_p2_animals):
         self.length = len(empty_board[0])
         self.height = len(empty_board)
         self.water = set()
@@ -58,6 +58,9 @@ class board:        # used for representing the initial board, does not include 
             for x,tile in enumerate(row):
                 if tile != "0":
                     {"1":self.water,"2":self.traps[1],"3":self.traps[2],"4":self.lairs[1],"5":self.lairs[2]}[tile].add((x,y))
+
+        self.animals = {1:initial_p1_animals,2:initial_p2_animals} #the choic of using a dict is for making selecting animals easier
+
 INITIAL_P2_ANIMALS = {(0,0):animal("Lion",2),           #the dictionaries contain the coordinates of every animal and their coordinates as the keys
                     (6,0):animal("Tiger",2),            #dictionaries are useful for checking if a tile has an animal and to know where a player has animals
                     (1,1):animal("Dog",2),              #In case putting everything in an array is preffered, we can make an interpreter that creates those dictionaries by reading the array
@@ -86,7 +89,25 @@ board0 = board(["0025200",
                 "0000000",
                 "0003000",
                 "0034300"
-                ])
+                ], INITIAL_P1_ANIMALS,INITIAL_P2_ANIMALS)
+
+board1 = board(["02520",
+                "00200",
+                "01010",
+                "00300",
+                "03430"
+                ],
+                {(4,4):animal("Lion",1),
+                (0,4):animal("Tiger",1),
+                (3,3):animal("Elephant",1),
+                (1,3):animal("Mouse",1)
+                }
+               ,
+               {(0,0):animal("Lion",2),
+                (4,0):animal("Tiger",2),
+                (1,1):animal("Elephant",2),
+                (3,1):animal("Mouse",2)}
+               )
 
 """                                 # Old program
 EMPTY_BOARD = ["..#0#..",              #The way I thought about displaying the board is by getting the empty board and pasting all the animals on it
@@ -112,9 +133,9 @@ LAIRS = {2:(3,0),1:(3,8)}
 
 
 class State():              #object with one state of the game
-    def __init__(self,board_,p1_animals,p2_animals):
+    def __init__(self,board_):
         self.board = board_
-        self.animals = {1:p1_animals,2:p2_animals}      #made for selecting animals easier
+        self.animals = self.board.animals   #made for selecting animals easier as the animals weren't originally stored in the board
         self.player = 1                 # player refers to the player who's going to make the next play
         self.available_moves = set()    # the format of the moves in the set is (start,dest)
         self.update_available_moves()   #func that is defined below
@@ -137,7 +158,7 @@ class State():              #object with one state of the game
                 self.winner = 3-self.player
 
     def __repr__(self):                 #used for converting into a string using the repr() function, may delete this because the code isn't very readable, a display function should do the trick
-        return "\n".join("".join(str(a[(x,y)]) if (x,y) in (a:=self.animals[1]) else str(a[(x,y)]) if (x,y) in (a:=self.animals[2]) else self.board.empty_board[y][x] for x in range(7)) for y in range(9))
+        return "\n".join("".join(str(a[(x,y)]) if (x,y) in (a:=self.animals[1]) else str(a[(x,y)]) if (x,y) in (a:=self.animals[2]) else self.board.empty_board[y][x] for x in range(self.board.length)) for y in range(self.board.height))
 
 
     def move(self,move):
